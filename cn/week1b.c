85ac15f408da84fe86507cd0dcbc2f3a6c79f52b
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-02-12 18:46:49 +0530

    Update week1b.c

--- cn/week1b.c
+++ cn/week1b.c
@@ -1,25 +1,23 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-int main()
-{
-char a[100],b[100],c[100];
-int i,j=0,n,k=0;
-printf("Enter the data");
-gets(a);
-n=strlen(a);
-for(i=0;i<n;i++){
-if((a[i]=='d'&&a[i+1]=='1'&&a[i+2]=='e')||(a[i]=='e'&&a[i+1]=='s'&&a[i+2]=='c')){
-c[k++]='e';
-c[k++]='s';
-c[k++]='c';
-}
-c[k++]=a[i];
-}
-c[k++]='\0';
-c[k++]='\0';
-scanf("%s\n",b);
-printf("DLESTX");
-scanf("%s",c);
-printf("DLEETX");
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char a[100], c[200]; 
+    int i, k = 0, n;
+    printf("Enter the data: ");
+    fgets(a, sizeof(a), stdin);
+    n = strlen(a);
+    if (a[n - 1] == '\n')  
+        a[n - 1] = '\0';
+    for (i = 0; i < n; i++) {
+        if ((a[i] == 'd' && a[i + 1] == '1' && a[i + 2] == 'e') || 
+            (a[i] == 'e' && a[i + 1] == 's' && a[i + 2] == 'c')) {
+            c[k++] = 'e';
+            c[k++] = 's';
+            c[k++] = 'c';
+        }
+        c[k++] = a[i];
+    }
+    c[k] = '\0';  
+    printf("\nDLESTX %s DLEETX\n", c);
+    return 0;
 }
