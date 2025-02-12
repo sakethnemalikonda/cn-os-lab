9ca38831a055c0e83a5bd37a492719ce9d720780
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-02-12 13:10:13 +0530

    Add files via upload

---
+++ cn/week1a.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a[15];
+  int i,j,k,n,c=0,pos=0;
+  printf("\n Enter the no.of bits");
+  for(i=0;i<n;i++){
+      scanf("%d",&n);
+      for(i=0;i<n;i++){
+        if(a[i]==1){
+          c++;
+          if(c==5){
+            pos=i+1;
+            c=0;
+            for(j=n;j>=pos;j--){
+              k=j+1;
+              a[k]=a[j];
+            }
+          a[pos]=0;
+          n=n+1;
+          else{
+            c=0;
+          }}
+printf("\n Data after stuffing");
+printf("011111110");
+for(i=0;i<n;i++){
+printf("%d",a[i]);
+}
+printf("011111110");
+getch();}
+}
+}
