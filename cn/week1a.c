5322bb8192159262f247599ed1190e17a87e6c7c
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-02-12 18:43:19 +0530

    Update week1a.c

--- cn/week1a.c
+++ cn/week1a.c
@@ -1,33 +1,29 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-  int a[15];
-  int i,j,k,n,c=0,pos=0;
-  printf("\n Enter the no.of bits");
-  for(i=0;i<n;i++){
-      scanf("%d",&n);
-      for(i=0;i<n;i++){
-        if(a[i]==1){
-          c++;
-          if(c==5){
-            pos=i+1;
-            c=0;
-            for(j=n;j>=pos;j--){
-              k=j+1;
-              a[k]=a[j];
+#include <stdio.h>
+void main() {
+    int a[30], b[50];
+    int i, j, n, c = 0, pos = 0, k = 0;
+    printf("Enter the number of bits: ");
+    scanf("%d", &n);
+    printf("Enter the bit stream (0s and 1s only):\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++) {
+        b[k++] = a[i];
+        if (a[i] == 1) {
+            c++;
+            if (c == 5) { 
+                b[k++] = 0;
+                c = 0; 
             }
-          a[pos]=0;
-          n=n+1;
-          else{
-            c=0;
-          }}
-printf("\n Data after stuffing");
-printf("011111110");
-for(i=0;i<n;i++){
-printf("%d",a[i]);
-}
-printf("011111110");
-getch();}
-}
+        } else {
+            c = 0; 
+        }
+    }
+    printf("\nData after Bit Stuffing: ");
+    printf("01111110 "); 
+    for (i = 0; i < k; i++) {
+        printf("%d", b[i]);
+    }
+    printf(" 01111110\n"); 
 }
