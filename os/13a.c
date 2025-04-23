007ed08d327feb38b4ccad394f4bdb9e46f63e45
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-04-23 12:27:19 +0530

    Update 13a.c

--- os/13a.c
+++ os/13a.c
@@ -52,3 +52,41 @@ j = log%pagesize;
 temp = base + (temp*pagesize)+j; //lhs 'temp' is physical address rhs and 'temp' is frame num
 printf("\nPhysical address is : %d",temp);
 }
+
+Example Input/Output:
+
+student@202-28:~/5x0$ cc 13a.c
+student@202-28:~/5x0$ ./a.out
+Enter the Process size: 8
+
+Enter the main memory size: 16
+
+Enter the page size: 2
+
+The process is divided into 4 pages
+
+The main memory is divided into 8 frames
+
+Enter the frame number of page 0: 5
+
+Enter the frame number of page 1: 6
+
+Enter the frame number of page 2: 2
+
+Enter the frame number of page 3: 3
+
+
+Frameno	pageno	ValidationBit
+------	------	----------
+ 0 	 -1 	 0
+ 1 	 -1 	 0
+ 2 	  2 	 1
+ 3 	  3 	 1
+ 4 	 -1 	 0
+ 5 	  0 	 1
+ 6 	  1 	 1
+ 7 	 -1 	 0
+Enter the logical address: 3
+
+Enter the base address: 1000
+Physical address is : 1013
